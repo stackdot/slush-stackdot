b491e5bbccbfa80405bc0669f23417f7b4ca8d90
Quinton Pike <qrpike@gmail.com>
2016-08-15 13:44:33 -0400

    bump for service again

--- templates/module/services/name.service.js
+++ templates/module/services/name.service.js
@@ -2,12 +2,21 @@
 module.exports = class <%=camelCased%>Service {
 	constructor( $http ){
 		console.log('New Service')
+		this.$http = $http
+		this.data = {
+			item: ''
+		}
 	}
 	get( callback ){
-		return $http({
+		return this.$http({
 			method: 'GET',
 			withCredentials: true,
 			url: 'api_url'
+		}).then(( res )=>{
+			this.data.item = res.data
+			callback( res.data )
+		}).catch(( err )=>{
+			console.log('Err', err)
 		})
 	}
 }
